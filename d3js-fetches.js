932985cc35517d2625a85e6f0180f5c8f8f96aa6
ivan-abreu <ivanabreuochoa@gmail.com>
2021-06-24 14:17:03 -0500

    1 file modified

--- d3js-fetches.js
+++ d3js-fetches.js
@@ -1,5 +1,5 @@
 let urljson = 'https://dog.ceo/api/breeds/image/random';
-let urlcsv = 'https://dog.ceo/api/breeds/image/random';
+let urlcsv = 'https://raw.githubusercontent.com/ivan-abreu/evd-d3js-fetches/master/religionok.csv';
 
 d3.json(urljson).then(function(data) {
   console.log( data )
@@ -9,7 +9,10 @@ d3.json(urljson).then(function(data) {
 
 d3.csv(urlcsv).then(function(data) {
   console.log( data )
-  d3.select("body").append('img')
-                   .attr('src', data.message)
+  data.map( (ele) => {
+    d3.select("body").append('div')
+                    .text(ele.indicador)
+  })
+  
 });
 
